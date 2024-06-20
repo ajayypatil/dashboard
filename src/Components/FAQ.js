39a8f123ad38dda041d720cc573e2d165ff59630
ajayypatil <yadava701@gmail.com>
2024-06-20 12:23:32 +0530

    FAQ Page

--- src/Components/FAQ.js
+++ src/Components/FAQ.js
@@ -1,8 +1,93 @@
+import { Accordion, Box, AccordionSummary , Typography, AccordionDetails} from '@mui/material'
 import React from 'react'
-
+import Header from './Header'
+import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
 const FAQ = () => {
   return (
-    <div>FAQ</div>
+    <Box m="20px">
+      <Header title="FAQ" subTitle="Frequently Asked Questions Page"/>
+      <Box>
+      <Accordion>
+        <AccordionSummary
+          expandIcon={<ArrowDropDownIcon />}
+          aria-controls="panel1-content"
+          id="panel1-header"
+        >
+          <Typography variant='h5'>Question 1</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography variant='h5'>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+            malesuada lacus ex, sit amet blandit leo lobortis eget.
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+
+      <Accordion>
+        <AccordionSummary
+          expandIcon={<ArrowDropDownIcon />}
+          aria-controls="panel1-content"
+          id="panel1-header"
+        >
+          <Typography variant='h5'>Question 2</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography variant='h5'>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+            malesuada lacus ex, sit amet blandit leo lobortis eget.
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+
+      <Accordion>
+        <AccordionSummary
+          expandIcon={<ArrowDropDownIcon />}
+          aria-controls="panel1-content"
+          id="panel1-header"
+        >
+          <Typography variant='h5'>Question 3</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography variant='h5'>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+            malesuada lacus ex, sit amet blandit leo lobortis eget.
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+
+      <Accordion>
+        <AccordionSummary
+          expandIcon={<ArrowDropDownIcon />}
+          aria-controls="panel1-content"
+          id="panel1-header"
+        >
+          <Typography variant='h5'>Question 4</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography variant='h5'>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+            malesuada lacus ex, sit amet blandit leo lobortis eget.
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+
+      <Accordion>
+        <AccordionSummary
+          expandIcon={<ArrowDropDownIcon />}
+          aria-controls="panel1-content"
+          id="panel1-header"
+        >
+          <Typography variant='h5'>Question 5</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography variant='h5'>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+            malesuada lacus ex, sit amet blandit leo lobortis eget.
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+      </Box>
+    </Box>
   )
 }
 
